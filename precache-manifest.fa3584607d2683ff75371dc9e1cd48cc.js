1d022004ea6af9632c90ecd259ffa3d3310247e3
PeralStudio <info@peralstudio.com>
2020-06-12 00:53:43 +0200

    Updates

---
+++ precache-manifest.fa3584607d2683ff75371dc9e1cd48cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7473f0e5ccd42109b4883542583e1047",
+    "url": "/WebPersonal/index.html"
+  },
+  {
+    "revision": "34f221243402eebf86c3",
+    "url": "/WebPersonal/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "00170c6be0cf013b2185",
+    "url": "/WebPersonal/static/css/main.f327b72e.chunk.css"
+  },
+  {
+    "revision": "34f221243402eebf86c3",
+    "url": "/WebPersonal/static/js/2.5cd9cb3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WebPersonal/static/js/2.5cd9cb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00170c6be0cf013b2185",
+    "url": "/WebPersonal/static/js/main.3ca9a938.chunk.js"
+  },
+  {
+    "revision": "34bda6242ab4cc10e3ff",
+    "url": "/WebPersonal/static/js/runtime-main.e730c21e.js"
+  }
+]);
