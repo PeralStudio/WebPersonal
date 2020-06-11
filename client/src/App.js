8905e438ab7d11ffef70ea4e35a019c06df7d230
PeralStudio <info@peralstudio.com>
2020-06-11 17:05:58 +0200

    Sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Web Personal-Client</h1>
+      <h2>Proyecto</h2>
     </div>
   );
 }
