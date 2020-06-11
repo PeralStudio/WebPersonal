926e74c5f6cbabadbf471b57de6f7b03c526e425
PeralStudio <info@peralstudio.com>
2020-06-11 17:19:55 +0200

    antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Web Personal-Client</h1>
       <h2>Proyecto</h2>
+      <DatePicker  />
     </div>
   );
 }
